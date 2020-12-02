19ffa031a395ce262f284119a5a71347c2180ca0
sunhuiquan <sunhuiquan@yeah.net>
2020-12-02 15:04:57 +0800

    get start

---
+++ get_cscope/abs_add.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string file_hdr = "/home/suen/Work/xv6_lab/xv6-labs-2020";
+
+int main()
+{
+	string t;
+	ifstream ifs;
+	ifs.open("find_files");
+
+	ofstream ofs;
+	ofs.open("../cscope.files");
+
+	while(ifs >> t)
+	{
+		t.erase(0,2);
+		ofs << (file_hdr + t) << "\n";
+	}
+
+	ifs.close();
+	ofs.close();
+
+	return 0;
+}
