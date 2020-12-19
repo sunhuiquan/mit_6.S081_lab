a184db3e32efbb8b5fbe08cd652a1ed490653e49
sunhuiquan <sunhuiquan@yeah.net>
2020-12-19 14:03:03 +0800

    finish

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,20 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  ++bstate.nthread;
+  if(bstate.nthread != nthread){
+	pthread_cond_wait(&bstate.barrier_cond,&bstate.barrier_mutex); 
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
+  else
+  {
+	bstate.nthread = 0;
+	++bstate.round;
+	pthread_cond_broadcast(&bstate.barrier_cond);
+	pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
 }
 
 static void *
@@ -43,7 +56,7 @@ thread(void *xa)
   for (i = 0; i < 20000; i++) {
     int t = bstate.round;
     assert (i == t);
-    barrier();
+	barrier();
     usleep(random() % 100);
   }
 
