07d6e0da394111a66f84677955f2b4502a60fa7e
sunhuiquan <sunhuiquan@yeah.net>
2021-10-03 23:04:46 +0800

    add feat: itrunc

--- kernel/fs.c
+++ kernel/fs.c
@@ -443,8 +443,8 @@ bmap(struct inode *ip, uint bn)
 void itrunc(struct inode *ip)
 {
 	int i, j;
-	struct buf *bp;
-	uint *a;
+	struct buf *bp, *bp2;
+	uint *a, *a2;
 
 	for (i = 0; i < NDIRECT; i++)
 	{
@@ -467,6 +467,32 @@ void itrunc(struct inode *ip)
 		brelse(bp);
 		bfree(ip->dev, ip->addrs[NDIRECT]);
 		ip->addrs[NDIRECT] = 0;
+	}
+
+	if (ip->addrs[NDIRECT + 1])
+	{
+		bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+		a = (uint *)bp->data;
+		for (j = 0; j < NINDIRECT; ++j)
+		{
+			if (a[j])
+			{
+				bp2 = bread(ip->dev, a[j]);
+				a2 = (uint *)bp2->data;
+
+				for (int k; k < NINDIRECT; ++k)
+				{
+					if (a2[k])
+						bfree(ip->dev, a2[k]);
+				}
+
+				brelse(bp2);
+				bfree(ip->dev, a[j]);
+			}
+		}
+		brelse(bp);
+		bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+		ip->addrs[NDIRECT + 1] = 0;
 	}
 
 	ip->size = 0;
